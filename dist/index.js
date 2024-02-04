e345e160ec1cb8e4832917f6062291cf53d76c27
Ubaidmwd <ubaidmwd@gmail.com>
2024-02-04 19:03:09 +0500

    example 5

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,32 @@
 "use strict";
 /*************data type************/
+// const func = (n, o) => {
+//   return { n, o };
+// };
+// const student = {
+//   name: "Ubaid Ullah",
+//   age: 24,
+// };
+// const student2 = {
+//   name: "Hizb Ullah",
+//   age: 22,
+//   phone: 1314122,
+// };
+// const res = func(student, student2);
+// console.log(res);
+
+//example 5
 const func = (n, o) => {
-    return { n, o };
+  return { n, o };
 };
 const student = {
-    name: "Ubaid Ullah",
-    age: 24,
+  name: "Ubaid Ullah",
+  age: 24,
 };
 const student2 = {
-    name: "Hizb Ullah",
-    age: 22,
-    phone: 1314122,
+  name: "Hizb Ullah",
+  age: 22,
+  phone: 1314122,
 };
 const res = func(student, student2);
 console.log(res);
-//example 5
